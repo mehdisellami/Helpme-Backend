a4193061fc6664ded101152d696729f375f547ed
mehdisellami <mehdisellami9@gmail.com>
2020-10-02 13:30:10 +0200

    init1

---
+++ src/test/java/com/application/helpme/HelpmeAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.application.helpme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpmeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
