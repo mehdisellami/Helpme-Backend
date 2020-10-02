3c6eda333e9382d07aaafaa83a270c67ea8be5fd
mehdisellami <mehdisellami9@gmail.com>
2020-10-02 12:26:48 +0200

    init

---
+++ HelpmeApp/src/main/java/com/application/helpme/HelpmeAppApplication.java
@@ -0,0 +1,13 @@
+package com.application.helpme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpmeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpmeAppApplication.class, args);
+	}
+
+}
