00e212123e6b1ff3ffd65bad7a5cf5495e9bbd95
mehdisellami <mehdisellami9@gmail.com>
2020-12-30 20:55:09 +0100

    heroku dep1

--- HelpmeApp/src/main/java/com/application/helpme/HelpmeAppApplication.java
+++ HelpmeApp/src/main/java/com/application/helpme/HelpmeAppApplication.java
@@ -7,6 +7,7 @@ import org.apache.catalina.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.application.Model.User;
@@ -14,7 +15,7 @@ import com.application.Model.User;
 
 @EntityScan(basePackages = {"com.application.Model"})
 @SpringBootApplication
-public class HelpmeAppApplication {
+public class HelpmeAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpmeAppApplication.class, args);
