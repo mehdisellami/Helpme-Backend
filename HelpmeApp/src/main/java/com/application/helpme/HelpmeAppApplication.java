957c3b25c0123794ab5e3d18b6316ab133b306c4
mehdisellami <mehdisellami9@gmail.com>
2020-12-30 18:55:28 +0100

    dep4

--- HelpmeApp/src/main/java/com/application/helpme/HelpmeAppApplication.java
+++ HelpmeApp/src/main/java/com/application/helpme/HelpmeAppApplication.java
@@ -7,11 +7,13 @@ import org.apache.catalina.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.application.Model.User;
 
 
 @EntityScan(basePackages = {"com.application.Model"})
+@ComponentScan({"com.application.lookek.controlleur"})
 @SpringBootApplication
 public class HelpmeAppApplication {
 
