2c5c22fc2b4d74788b76ebfaa33fe2ab901ec2c4
mehdisellami <mehdisellami9@gmail.com>
2021-02-01 00:32:25 +0100

    v1.2.3

---
+++ HelpmeApp/src/main/java/com/application/helpme/Model/Pref.java
@@ -0,0 +1,35 @@
+package com.application.helpme.Model;
+
+public class Pref {
+	
+	private boolean vieux;
+	
+	private boolean jeune;
+
+	public Pref() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isVieux() {
+		return vieux;
+	}
+
+	public void setVieux(boolean vieux) {
+		this.vieux = vieux;
+	}
+
+	public boolean isJeune() {
+		return jeune;
+	}
+
+	public void setJeune(boolean jeune) {
+		this.jeune = jeune;
+	}
+	
+	
+	
+	
+	
+
+}
