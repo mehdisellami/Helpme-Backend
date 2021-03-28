b136ddd1e74d1f835aa7d62701b10f55c6855197
SUPREMEchelson <chelson.sm@gmail.com>
2021-03-29 00:08:55 +0200

    fix_test

--- HelpmeApp/src/test/java/com/application/helpme/HelpmeAppApplicationTests.java
+++ HelpmeApp/src/test/java/com/application/helpme/HelpmeAppApplicationTests.java
@@ -1,15 +1,42 @@
 package com.application.helpme;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.runner.RunWith;
+import com.application.helpme.controlleur.*;
 @SpringBootTest
-public class HelpmeAppApplicationTests {
+@AutoConfigureMockMvc
 
+public class HelpmeAppApplicationTests {
+	@Autowired
+    private MockMvc mockMvc;
+	
 	@Test
- public	void contextLoads() {
+	public void getmission() throws Exception{
+		 mockMvc.perform(get("/getmission/3"))
+         .andExpect(status().isOk())
+         .andExpect(jsonPath("nomMission", is("Aller En COURS")));
+	}
+	
+	@Test
+	public void getlistmission() throws Exception{
+		 mockMvc.perform(get("/listeMission"))
+         .andExpect(status().isOk())
+       
+	}
+	
+	@Test
+	public void getuser() throws Exception{
+		 mockMvc.perform(get("/listeUser"))
+         .andExpect(status().isOk());
+		
 	}
 
 }
